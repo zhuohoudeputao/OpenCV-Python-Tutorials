20cf2bff1767f60dcd79662ad3304b6ce4adfe9b
zhuohoudeputao <35682614+zhuohoudeputao@users.noreply.github.com>
2019-08-26 13:14:11 +0800

    Take a break

---
+++ ImageProcessing/InteractiveForegroundExtractionusingGrabCutAlgorithm.py
@@ -0,0 +1,14 @@
+#%% import
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+#%% 
+img = cv.imread('images3/1.jpg')
+mask = np.zeros(img.shape[:2],np.uint8)
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+rect = (600,0,700,1200)
+cv.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis]
+plt.imshow(img),plt.colorbar(),plt.show()
